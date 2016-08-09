60c4e365ed9e382e38e2e2f33f7f775e1896646b
dfox <dave@cantinaconsulting.com>
2016-08-09 11:48:50 -0400

    Removing example dir so we only have one node_modules Updated example test

--- tests/example.js
+++ tests/example.js
@@ -1,13 +1,12 @@
 module.exports = {
   'Example test' : function (browser) {
       browser
-          .url('http://localhost:8080/notes/my-note')
+          .url('http://localhost:8082/notes/my-note')
           .waitForElementVisible('body', 1000)
-          .setValue('input[id=contents]', 'This is my note')
-          .waitForElementVisible('button[id=save]', 1000)
-          .click('button[id=save]')
+          .setValue('input[id=note-content]', 'This is my note')
+          .click('button[id=save-button]')
           .pause(1000)
-          .assert.containsText('#note', 'This is my note')
+          .assert.containsText('#status', 'Saved note: my-note')
           .assert.remote("co.cantina.junit.example.ExampleTest", "noteSaved")
           .end();
   }
